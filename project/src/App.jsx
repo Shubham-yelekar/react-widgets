028a3761ac6debcdf9bbfeea81d30d1261567fdf
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2025-01-15 08:59:16 +0530

    Update App.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -6,9 +6,13 @@ function App() {
 
   return (
     <div className=' md:h-screen flex items-center justify-center '>
-      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 '>
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-1 '>
         <Clock/>
         {/* <Timer/> */}
+      </div>
+      <div className='absolute bottom-4 flex gap-8'>
+        <a className='underline' href="https://github.com/Shubham-yelekar/react-widgets" target='_blank'>Project repo ⬈</a>
+        <a className='underline' href="https://shubhamyelekar.vercel.app/" target='_blank'>My website ⬈</a>
       </div>
     </div>
   )
